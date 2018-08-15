78be86b744dded2ec9fa636eb371979bb22cb2c5
Giselle Pacheco <gpacheco003@gmail.com>
2018-08-14 20:50:36 -0400

    added an alternative to reversing the array, and added greetAliens

--- codecademy.js
+++ codecademy.js
@@ -6,3 +6,18 @@ const reverseArray = (arr) => {
   }
   return reversed;
 };
+
+const reversedArray = (arr) => {
+  const reversed = [];
+  for(let i = arr.length - 1; i >= 0; i--){
+    reversed[reversed.length] = arr[i];
+  }
+  return reversed;
+};
+
+// greet aliens
+const greetAliens = (arr) => {
+  for(let i = 0; i < arr.length; i++){
+    console.log(`Oh powerful ${arr[i]}, we humans offer our unconditional surrender!`)
+  }
+}
