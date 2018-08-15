574e224c5a371ae38114b7adc49a03d582f4f208
Giselle Pacheco <gpacheco003@gmail.com>
2018-08-14 21:11:54 -0400

    converted everything to baby [animal] and also fixed the code for the fourth problem

--- codecademy.js
+++ codecademy.js
@@ -21,3 +21,31 @@ const greetAliens = (arr) => {
     console.log(`Oh powerful ${arr[i]}, we humans offer our unconditional surrender!`)
   }
 }
+
+const convertToBaby = (arr) => {
+  arr.forEach((element, i) => {
+    arr[i] = `baby ${arr[i]}`;
+  });
+  return arr;
+}
+
+const numbers = [5, 3, 9, 30];
+
+const smallestPowerOfTwo = arr => {
+      let results = [];
+      // The 'outer' for loop - loops through each element in the array
+      for (let i = 0; i < arr.length; i++) {
+            number = arr[i];
+
+            // The 'inner' while loop - searches for smallest power of 2 greater than the given number
+            let j = 1;
+            while (j < number) {
+                  j = j * 2;
+            }
+            results.push(j);
+      }
+      return results
+}
+
+console.log(smallestPowerOfTwo(numbers))
+// Should print the returned array [ 8, 4, 16, 32 ] instead prints the returned array [8]
